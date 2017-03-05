695311a1419ae4918964ede3c69724bfceb32bab
Antihayp <zayzsacha@mail.ru>
2017-03-05 21:00:45 +0700

    Test int vector free

--- src/IntVector.c
+++ src/IntVector.c
@@ -4,25 +4,22 @@
 
 IntVector *int_vector_new(size_t initial_capacity){
 	IntVector *v = malloc(sizeof(IntVector));
-
 	v->data = malloc(sizeof(int) * initial_capacity);
 	v->size = 0;
 	v->capacity = initial_capacity;
-
 	return v;	
 }
 
 IntVector *int_vector_copy(const IntVector *v){
 	IntVector *copy_v = malloc(sizeof(IntVector));
-
 	copy_v->data = malloc(sizeof(int) * v->capacity);
 	memcpy(copy_v->data, v->data, sizeof(int) * v->capacity);
 	copy_v->size = v->size;
 	copy_v->capacity = v->capacity;
-
 	return copy_v;
 }
 
 void int_vector_free(IntVector *v){
+	free(v->data);
 	free(v);
 }
--- src/main.c
+++ src/main.c
@@ -7,7 +7,6 @@ int main()
 	size_t initial_capacity = 5;
 
 	IntVector *v = int_vector_new(initial_capacity);
-	printf("Capacity = %d Size = %d Data = %p\n", v->capacity, v->size, v->data);
 	
 	for(int i = 0; i < initial_capacity; i++){
 		v->data[i] = i;
@@ -20,15 +19,21 @@ int main()
 	printf("Capacity = %d Size = %d Data = %p\n", v->capacity, v->size, v->data);
 
 	IntVector *copy_v = int_vector_copy(v);
+
 	printf("Capacity = %d Size = %d Data = %p\n", copy_v->capacity, copy_v->size, copy_v->data);
 
-	for(int i = 0; i < initial_capacity; i++){
+	for(int i = 0; i < copy_v->size; i++){
 		printf("%d", copy_v->data[i]);
 	}
 	printf("\n");
 
 	int_vector_free(v);
+
 	printf("Capacity = %d Size = %d Data = %p\n", v->capacity, v->size, v->data);
+	for(int i = 0; i < v->capacity; i++){
+		printf("%d", v->data[i]);
+	}
+	printf("\n");
 
 	return 0;
 }
