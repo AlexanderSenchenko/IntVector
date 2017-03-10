9f551f6b43695f65b76474ef8a1d629beaa023e5
Александр <zayzsacha@mail.ru>
2017-03-10 12:38:47 +0700

    Finish commit(but it isn't exactly)

--- src/IntVector.c
+++ src/IntVector.c
@@ -2,13 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 
-IntVector *int_vector_new(size_t initial_capacity){
+IntVector *int_vector_new(size_t initial_capacity)
+{
 	IntVector *v = malloc(sizeof(IntVector));
-	if (v == NULL){
+	if (v == NULL) {
 		return NULL;
 	}
 	v->data = malloc(sizeof(int) * initial_capacity);
-	if (v->data == NULL){
+	if (v->data == NULL) {
 		free(v);
 		return NULL;
 	}
@@ -17,13 +18,14 @@ IntVector *int_vector_new(size_t initial_capacity){
 	return v;
 }
 
-IntVector *int_vector_copy(const IntVector *v){
+IntVector *int_vector_copy(const IntVector *v)
+{
 	IntVector *copy_v = malloc(sizeof(IntVector));
-	if (copy_v == NULL){
+	if (copy_v == NULL) {
 		return NULL;
 	}
 	copy_v->data = malloc(sizeof(int) * int_vector_get_capacity(v));
-	if (copy_v->data == NULL){
+	if (copy_v->data == NULL) {
 		free(copy_v);
 		return NULL;
 	}
@@ -33,41 +35,47 @@ IntVector *int_vector_copy(const IntVector *v){
 	return copy_v;
 }
 
-void int_vector_free(IntVector *v){
-	if (v != NULL){
+void int_vector_free(IntVector *v)
+{
+	if (v != NULL) {
 		free(v->data);
 		free(v);
 	}
 }
 
-int int_vector_get_item(const IntVector *copy_v, size_t index){
-	if (index < int_vector_get_capacity(copy_v)){
+int int_vector_get_item(const IntVector *copy_v, size_t index)
+{
+	if (index < int_vector_get_capacity(copy_v)) {
 		return copy_v->data[index];
 	}
 	return 0;
 }
 
-void int_vector_set_item(IntVector *copy_v, size_t index, int item){
-	if (item < int_vector_get_capacity(copy_v)){
+void int_vector_set_item(IntVector *copy_v, size_t index, int item)
+{
+	if (item < int_vector_get_capacity(copy_v)) {
 		copy_v->data[index] = item;
 	}
 }
 
-size_t int_vector_get_size(const IntVector *copy_v){
+size_t int_vector_get_size(const IntVector *copy_v)
+{
 	return copy_v->size;
 }
 
-size_t int_vector_get_capacity(const IntVector *copy_v){
+size_t int_vector_get_capacity(const IntVector *copy_v)
+{
 	return copy_v->capacity;
 }
 
-int int_vector_push_back(IntVector *copy_v, int item){
+int int_vector_push_back(IntVector *copy_v, int item)
+{
 	if (int_vector_get_capacity(copy_v) == 0){
 		copy_v->capacity = 1;
 	}
-	if (int_vector_get_capacity(copy_v) == int_vector_get_size(copy_v)){
+	if (int_vector_get_capacity(copy_v) == int_vector_get_size(copy_v)) {
 		int *test = realloc(copy_v->data, sizeof(int) * int_vector_get_capacity(copy_v) * 2);
-		if (test == NULL){
+		if (test == NULL) {
 			return -1;
 		}
 		copy_v->capacity = int_vector_get_capacity(copy_v) * 2;
@@ -79,16 +87,18 @@ int int_vector_push_back(IntVector *copy_v, int item){
 	return 0;
 }
 
-void int_vector_pop_back(IntVector *copy_v){
-	if (int_vector_get_size(copy_v) != 0){
+void int_vector_pop_back(IntVector *copy_v)
+{
+	if (int_vector_get_size(copy_v) != 1) {
 		copy_v->size--;
 	}
 }
 
-int int_vector_shrink_to_fit(IntVector *copy_v){
-	if (int_vector_get_capacity(copy_v) > int_vector_get_size(copy_v)){
+int int_vector_shrink_to_fit(IntVector *copy_v)
+{
+	if (int_vector_get_capacity(copy_v) > int_vector_get_size(copy_v)) {
 		int *test = realloc(copy_v->data, sizeof(int) * int_vector_get_size(copy_v));
-		if (test == NULL){
+		if (test == NULL) {
 			return -1;
 		}
 		copy_v->data = test;
@@ -97,10 +107,11 @@ int int_vector_shrink_to_fit(IntVector *copy_v){
 	return 0;
 }
 
-int int_vector_resize(IntVector *copy_v, size_t new_size){
-	if (new_size > int_vector_get_size(copy_v)){
-		for (int i = int_vector_get_size(copy_v); i < new_size; i++){
-			if (int_vector_push_back(copy_v, 0) == -1){
+int int_vector_resize(IntVector *copy_v, size_t new_size)
+{
+	if (new_size > int_vector_get_size(copy_v)) {
+		for (int i = int_vector_get_size(copy_v); i < new_size; i++) {
+			if (int_vector_push_back(copy_v, 0) == -1) {
 				return -1;
 			}
 		}
@@ -109,14 +120,16 @@ int int_vector_resize(IntVector *copy_v, size_t new_size){
 	return 0;
 }
 
-int int_vector_reserve(IntVector *copy_v, size_t new_capacity){
-	if (new_capacity > copy_v->capacity){	
+int int_vector_reserve(IntVector *copy_v, size_t new_capacity)
+{
+	if (new_capacity > copy_v->capacity) {	
 		copy_v->capacity = new_capacity;
 		// FIXME
-		copy_v->data = realloc(copy_v->data, sizeof(int) * copy_v->capacity);
-		if (copy_v->data == NULL){
+		int *test = realloc(copy_v->data, sizeof(int) * copy_v->capacity);
+		if (test == NULL) {
 			return -1;
 		}
+		copy_v->data = test;
 	}
 	return 0;
 }
--- src/main.c
+++ src/main.c
@@ -8,22 +8,16 @@ int main()
 
 //int_vector_new
 	IntVector *v = int_vector_new(initial_capacity);
-	if (v == NULL){
-		int_vector_free(v);
-	}
 	for (int i = 0; i < 5; i++){
 		int_vector_push_back(v, i + 1);
 	}
 
 //int_vector_copy
 	IntVector *copy_v = int_vector_copy(v);
-	if (copy_v == NULL){
-		int_vector_free(copy_v);
-	}
 	for (int i = 0; i < int_vector_get_size(copy_v); i++){
 		printf("%d ", v->data[i]);
 	}
-	printf("\nSize = %d Capacity = %d\n", int_vector_get_size(copy_v), int_vector_get_capacity(copy_v));
+	printf("\nSize = %d Capacity = %d\n", (int) int_vector_get_size(copy_v), (int) int_vector_get_capacity(copy_v));
 
 //int_vector_free
 	int_vector_free(v);
@@ -47,11 +41,11 @@ int main()
 	for (int i = 0; i < int_vector_get_size(copy_v); i++){
 		printf("%d ", copy_v->data[i]);
 	}
-	printf("\nSize = %d Capacity = %d\n", int_vector_get_size(copy_v), int_vector_get_capacity(copy_v));
+	printf("\nSize = %d Capacity = %d\n", (int) int_vector_get_size(copy_v), (int) int_vector_get_capacity(copy_v));
 
 //int_vector_pop_back
 	int_vector_pop_back(copy_v);
-	printf("Capacity = %d Size = %d\n", int_vector_get_capacity(copy_v), int_vector_get_size(copy_v));
+	printf("Capacity = %d Size = %d\n", (int) int_vector_get_capacity(copy_v), (int) int_vector_get_size(copy_v));
 	for (int i = 0; i < copy_v->size; i++){
 		printf("%d ", copy_v->data[i]);
 	}
@@ -59,25 +53,24 @@ int main()
 
 //int vector shrinck to fit
 	printf("%d\n",int_vector_shrink_to_fit(copy_v));
-	printf("Capacity = %d Size = %d\n", int_vector_get_capacity(copy_v), int_vector_get_size(copy_v));
+	printf("Capacity = %d Size = %d\n", (int) int_vector_get_capacity(copy_v), (int) int_vector_get_size(copy_v));
 
 //int_vector_resize
 	size_t new_size = 15;
 	printf("%d\n",int_vector_resize(copy_v, new_size));
-	printf("Capacity = %d Size = %d\n", int_vector_get_capacity(copy_v), int_vector_get_size(copy_v));
+	printf("Capacity = %d Size = %d\n", (int) int_vector_get_capacity(copy_v), (int) int_vector_get_size(copy_v));
 	for (int i = 0; i < int_vector_get_size(copy_v); i++){
 		printf("%d ", copy_v->data[i]);
 	}
 	printf("\n");
-	return 0;
 
 //int_vector_reserve
-	printf("\tint_vector_reserve\n");
-
 	size_t new_capacity = 20;
 
 	printf("%d\n", int_vector_reserve(copy_v, new_capacity));
-	printf("Capacity = %d Size = %d Data = %p\n", copy_v->capacity, copy_v->size, copy_v->data);
+	printf("Capacity = %d Size = %d\n", (int) int_vector_get_capacity(copy_v), (int) int_vector_get_size(copy_v));
+
+	int_vector_free(copy_v);
 
 	return 0;
 }
